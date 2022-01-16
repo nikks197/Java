c31a7c50d8b62f2cfb97b3bff44f0b3a5f0b1730
Nikhil <nikks197@gmail.com>
2022-01-16 17:41:40 +0530

    first java code: sum of two numbers

---
+++ nikhil/src/main/java/nikhil/SumOfTwoNumbers.java
@@ -0,0 +1,17 @@
+package nikhil;
+
+import java.util.*;
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c;
+		c = a+b;
+		System.out.println(c);
+
+	}
+
+}
